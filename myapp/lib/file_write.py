608e6153804c4a327f82a6cd8060a94acea9136a
Artemka0101 <64738836+Artemka0101@users.noreply.github.com>
2023-04-22 17:32:17 +0300

    Create file_write.py

---
+++ myapp/lib/file_write.py
@@ -0,0 +1,17 @@
+def file_write():
+    file_name = "maze"+str(datetime.now().date())+"-"+str(datetime.now().hour)+str(datetime.now().minute)+str(datetime.now().second)+".maze"
+    file = open(file_name, 'wb+')
+    bt = ("IHATEPYTHON!"+'\n').encode()
+    file.write(bt)
+    bt = (str(height)+'\n').encode()
+    file.write(bt)
+    bt = (str(width) + "\n").encode()
+    file.write(bt)
+    for i in range(len(g)):
+        for j in range(len(g[i])):
+            bt = (str(g[i][j])+"\n").encode()
+            file.write(bt)
+        bt = (str(-1) + "\n").encode()
+        file.write(bt)
+    bt = (str(-2) + "\n").encode()
+    file.write(bt)
